510c2ace81650b85de4dc92ffa130a7f3078ab9a
Pavlo Hodysh <gaarasmail@gmail.com>
2016-11-13 06:36:22 +0200

    removed empty redundant project

--- Silent.Practices.DDD.Contracts/ICommandHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using Silent.Practices.Patterns;
-
-namespace Silent.Practices.Domain.Contracts
-{
-    public interface ICommandHandler<in TCommand> : IHandler<TCommand> where TCommand : Command
-    {
-    }
-}
